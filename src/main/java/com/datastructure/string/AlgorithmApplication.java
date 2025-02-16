1001c6b72d470f6b653b0454e1bac890f211c9c2
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-02-15 23:47:58 -0300

    feature/array: bigger seller amount using comparator

--- src/main/java/com/datastructure/string/AlgorithmApplication.java
+++ src/main/java/com/datastructure/string/AlgorithmApplication.java
@@ -3,20 +3,49 @@ package com.datastructure.string;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 @SpringBootApplication
 public class AlgorithmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlgorithmApplication.class, args);
-        System.out.println(dotProductTwoArrays(new int[]{1,0,0,2,3}, new int[]{0,3,0,4,0}));
+        List<Seller> sellers = new ArrayList<>(List.of(
+                new Seller("Barry Allen", 18196.0),
+                new Seller("Logan", 4255.0),
+                new Seller("Maria", 10298.0),
+                new Seller("Ana", 26485.0),
+                new Seller("Aurora", 19982.0),
+                new Seller("Noah", 15820.0),
+                new Seller("Leo", 21412.0),
+                new Seller("Alex", 12000.0)
+        ));
+        System.out.println(biggerSellerAmount(sellers));
     }
 
-    public static int dotProductTwoArrays(int[] nums, int[] nums2) {
-        int value = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = nums[i] * nums2[i];
-            value = value + sum;
-        }
-        return value;
+    public static String biggerSellerAmount(List<Seller> sellers) {
+        sellers.sort(Comparator.comparingDouble(Seller::getAmount).reversed());
+        return sellers.get(0).getName();
     }
+}
+
+class Seller{
+    private String name;
+    private double amount;
+
+    public Seller(String name, double amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
 }
