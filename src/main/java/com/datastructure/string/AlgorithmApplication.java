ccb42179a31fc66c48cd578d98bee15cb5fbef07
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-03-26 10:22:47 -0300

    feature/reverseList: tail recursive

--- src/main/java/com/datastructure/string/AlgorithmApplication.java
+++ src/main/java/com/datastructure/string/AlgorithmApplication.java
@@ -3,7 +3,6 @@ package com.datastructure.string;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,17 +14,14 @@ public class AlgorithmApplication {
     }
 
     public static <T> List<T> reverse(List<T> list) {
-        List<T> newList = new ArrayList<>();
-
-        return reverseList(list, newList, list.size());
-    }
-
-    private static <T> List<T> reverseList(List<T> list, List<T> anotherList, int number){
-        if (number == 0) {
-            return anotherList;
+        if (list.size() <= 1) {
+            return list;
         }
 
-        anotherList.add(list.get(number - 1));
-        return reverseList(list, anotherList, number - 1);
+        T head = list.get(0);
+        List<T> list2 = list.subList(1, list.size());
+        reverse(list2).add(head);
+
+        return list2;
     }
 }
