090e6b22aad79b7c286474e19b99e74efc8bcd00
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-03-30 18:53:01 -0300

    feature/bubbleSort: bubble sort

--- src/main/java/com/datastructure/string/AlgorithmApplication.java
+++ src/main/java/com/datastructure/string/AlgorithmApplication.java
@@ -10,17 +10,24 @@ public class AlgorithmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlgorithmApplication.class, args);
-        System.out.println(Arrays.toString(bubbleSort(new int[]{0, 42, 32, 44, 64, 75, 86, 97})));
+        System.out.println(Arrays.toString(bubbleSort(new int[]{0, 42, 32, 44, 64, 75, -2, 97})));
     }
 
     public static int[] bubbleSort(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            int number = nums[i];
-            int nextNumber = nums[i + 1];
+        for (int i = 0; i < nums.length; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < nums.length -1 - i; j++) {
+                int number = nums[j];
+                int nextNumber = nums[j + 1];
 
-            if (number > nextNumber) {
-                nums[i + 1] = number;
-                nums[i] = nextNumber;
+                if (number > nextNumber) {
+                    nums[j + 1] = number;
+                    nums[j] = nextNumber;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
             }
         }
 
