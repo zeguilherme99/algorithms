9ca525023c76b05162e95becd1791a4098b451d2
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-04-01 11:35:25 -0300

    feature/mergeSort: merge sort

--- src/main/java/com/datastructure/string/AlgorithmApplication.java
+++ src/main/java/com/datastructure/string/AlgorithmApplication.java
@@ -10,20 +10,53 @@ public class AlgorithmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlgorithmApplication.class, args);
-        System.out.println(Arrays.toString(insertionSort(new int[]{0, 32, 42, -2, 64, 4, -3, 97})));
+        int[] arr = new int[]{100, 32, 42, -2, 64, 4, -3, 97};
+        mergeSortRecursive(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static int[] insertionSort(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            int index = i;
-            int aux = nums[i];
-            for (int j = i - 1; j >= 0 && nums[j] > aux; j--) {
-                nums[j + 1] = nums[j];
-                index = j;
+    public static void mergeSortRecursive(int[] nums, int first, int last) {
+        if (first < last) {
+            int middle = (first + last) / 2;
+            mergeSortRecursive(nums, first, middle);
+            mergeSortRecursive(nums, middle + 1, last);
+            merge(nums, first, middle, last);
+        }
+    }
+
+    public static void merge(int[] nums, int first, int middle, int last) {
+        int length = last - first + 1;
+        int i = first;
+        int j = middle + 1;
+        int k = 0;
+        int[] arr = new int[length];
+
+        while (i <= middle && j <= last) {
+            if (nums[i] < nums[j]) {
+                arr[k] = nums[i];
+                i++;
+            } else {
+                arr[k] = nums[j];
+                j++;
             }
-            nums[index] = aux;
+            k++;
         }
 
-        return nums;
+        while (i <= middle) {
+            arr[k] = nums[i];
+            i++;
+            k++;
+        }
+
+        while (j <= last) {
+            arr[k] = nums[j];
+            j++;
+            k++;
+        }
+
+        for (int c = 0; c < length; c++) {
+            nums[first + c] = arr[c];
+        }
     }
 }
