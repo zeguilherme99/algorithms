5d009ae3305d098e52317ea846ebe14b148a1414
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-08-24 14:12:04 -0300

    feature/graphs: counting components

--- src/main/java/com/datastructure/graphs/bfs/BfsGraph.java
+++ src/main/java/com/datastructure/graphs/bfs/BfsGraph.java
@@ -27,8 +27,7 @@ public class BfsGraph {
         return this.adjList[v];
     }
 
-    public void bfs(int vertex) {
-        boolean[] visited = new boolean[this.vertex];
+    public void bfs(int vertex, boolean[] visited) {
         visited[vertex] = true;
 
         Queue<Integer> queue = new ArrayDeque<>();
@@ -46,20 +45,34 @@ public class BfsGraph {
         }
     }
 
-    public static void main(String[] args) {
-        BfsGraph graph = new BfsGraph(6);
-        graph.addEdge(0,1);
-        graph.addEdge(0,5);
-        graph.addEdge(1,2);
-        graph.addEdge(1,3);
-        graph.addEdge(1,5);
-        graph.addEdge(2,3);
-        graph.addEdge(2,5);
-        graph.addEdge(3,4);
-        graph.addEdge(3,5);
-        graph.addEdge(4,5);
+    public int conectedComponents() {
+        int count = 0;
+        boolean[] visited = new boolean[this.vertex];
 
-        graph.bfs(1);
+        for (int i = 0; i < this.vertex; i++) {
+            if (!visited[i]) {
+                bfs(i, visited);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        BfsGraph graph = new BfsGraph(7);
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(2,6);
+        graph.addEdge(3,4);
+//        graph.addEdge(1,5);
+//        graph.addEdge(2,3);
+//        graph.addEdge(2,5);
+//        graph.addEdge(3,4);
+//        graph.addEdge(3,5);
+//        graph.addEdge(4,5);
+
+        System.out.println(graph.conectedComponents());
     }
 
 }
